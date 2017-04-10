1aeaee31a69efaef7c624c253b3097b1896d13eb
Алёна Крипец < >
2017-04-10 16:02:24 +0300

    почти все сделал, есть небольшой баг

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btOpenFile = new System.Windows.Forms.Button();
+            this.btChangeFile = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btOpenFile
+            // 
+            this.btOpenFile.Location = new System.Drawing.Point(23, 50);
+            this.btOpenFile.Name = "btOpenFile";
+            this.btOpenFile.Size = new System.Drawing.Size(121, 23);
+            this.btOpenFile.TabIndex = 2;
+            this.btOpenFile.Text = "Открыть файл";
+            this.btOpenFile.UseVisualStyleBackColor = true;
+            this.btOpenFile.Click += new System.EventHandler(this.btOpenFile_Click);
+            // 
+            // btChangeFile
+            // 
+            this.btChangeFile.Location = new System.Drawing.Point(234, 50);
+            this.btChangeFile.Name = "btChangeFile";
+            this.btChangeFile.Size = new System.Drawing.Size(143, 23);
+            this.btChangeFile.TabIndex = 3;
+            this.btChangeFile.Text = "Преобразовать";
+            this.btChangeFile.UseVisualStyleBackColor = true;
+            this.btChangeFile.Click += new System.EventHandler(this.btChangeFile_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(23, 8);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(201, 36);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(234, 8);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(226, 36);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 255);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btChangeFile);
+            this.Controls.Add(this.btOpenFile);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button btOpenFile;
+        private System.Windows.Forms.Button btChangeFile;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace spz_lb1_v6
 {
@@ -15,6 +16,52 @@ namespace spz_lb1_v6
         public Form1()
         {
             InitializeComponent();
+        }
+        int[] nums;
+        private void btOpenFile_Click(object sender, EventArgs e)
+        {
+            var file = new OpenFileDialog();
+            var sb = new StringBuilder();
+
+            if (file.ShowDialog() == DialogResult.OK && file.FileName != null)
+            {
+                using (var Reader = new StreamReader(file.FileName))      //создает поток для записи данных
+                                                                    // using (var Reader = new StreamReader("C:\\task.txt"))
+                {
+                    richTextBox1.Text = Reader.ReadToEnd();     //запись в поток
+
+                    sb.Append(richTextBox1.Text).ToString();
+                }
+
+            }
+            string newSb = sb.ToString().Trim();
+            string[] str = newSb.ToString().Split(' ');
+            nums = str
+                    .Select(n => int.Parse(n.Trim()))
+                    .ToArray();
+        }
+
+        private void btChangeFile_Click(object sender, EventArgs e)
+        {
+            string aaa = "";
+            int[] nums1 = new int[nums.Length - 1];
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (i == 0 || i == nums.Length - 1)//fixed
+                {
+                    nums1[i] = nums[i];
+                }
+                else
+                {
+                    nums1[i] = (nums[i - 1] + nums[i] + nums[i + 1]) / 3;
+                }
+            }
+           for(int i = 0; i < nums1.Length; i++)
+            {
+                aaa += nums1[i];
+                aaa += ' ';
+            }
+            richTextBox2.Text = aaa;
         }
     }
 }
